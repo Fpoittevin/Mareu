0b91fe4d4fa733b0cc55fa2391bafa024d5a2a73
Fpoittevin <dev@francoispoittevin.Fr>
2019-11-09 18:20:14 +0100

    creation of DI class

---
+++ app/src/main/java/com/ocr/francois/mareu/di/DI.java
@@ -0,0 +1,10 @@
+package com.ocr.francois.mareu.di;
+
+import com.ocr.francois.mareu.service.FakeMeetingApiService;
+import com.ocr.francois.mareu.service.MeetingApiService;
+
+public class DI {
+    private static MeetingApiService service = new FakeMeetingApiService();
+
+    public static MeetingApiService getMeetingApiService() { return service; }
+}
